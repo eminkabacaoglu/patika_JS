b73c63a25e15ef80cfba37957340f37d375bb37f
Emin Kabacaoglu <eminkabacaoglu@gmail.com>
2022-04-15 00:25:31 +0300

    Add files via upload

---
+++ clock_odev/js/clock.js
@@ -0,0 +1,18 @@
+let myName = prompt("Adınız Nedir? ")
+let adAlani = document.querySelector("span#myName")
+
+
+adAlani.innerHTML = myName;
+
+function showTime(){
+    let daysinWeek=["Pazar","Pazartesi","Salı","Çarşamba","Perşembe","Cuma","Cumartesi"]//js de hafta pazardan başlar
+    let time =new Date();
+    let hour = time.getHours();
+    let min = time.getMinutes();
+    let sec = time.getSeconds();
+    let day = daysinWeek[time.getDay()];
+    time = `${hour}:${min}:${sec} ${day}`;
+    document.getElementById("myClock").innerHTML=time
+}
+setInterval(showTime,1000);
+
